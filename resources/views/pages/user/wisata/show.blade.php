0874cca4df3fadb3859a39dfb43f25fd11d38b88
Muzakir Nur <muzakir.nurr@gmail.com>
2024-01-07 23:54:20 +0700

    fixing ulasan

--- resources/views/pages/user/wisata/show.blade.php
+++ resources/views/pages/user/wisata/show.blade.php
@@ -70,6 +70,9 @@
                             </svg>
                             @endfor
                         </div>
+                        <div class="w-full">
+                            <p class="text-sm font-extralight italic">{{ date('d F Y', strtotime($row->tanggal)) }}</p>
+                        </div>
                     </div>
                     <blockquote>
                         <p class="text-lg font-semibold text-gray-900 dark:text-white">"{{ $row->ulasan }}"</p>
@@ -104,6 +107,9 @@
                             <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
                         </svg>
                         @endfor
+                    </div>
+                    <div class="w-full">
+                        <p class="text-sm font-extralight italic">{{ date('d F Y', strtotime($userHadReview->tanggal)) }}</p>
                     </div>
                 </div>
                     <blockquote>
