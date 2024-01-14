994b96299add8edceebff944e81bf13e83055a94
Muzakir Nur <muzakir.nurr@gmail.com>
2024-01-14 12:41:36 +0700

    Menambahkan Visi Misi dan Struktur Organisasi

--- resources/views/pages/user/profile/struktur.blade.php
+++ resources/views/pages/user/profile/struktur.blade.php
@@ -1,3 +1,8 @@
 <x-guest-layout>
-    <h1 class="text-4xl">Ini Struktur Organisasi</h1>
+    <div class="w-full lg:w-1/2 mx-auto mb-8">
+        <h1 class="font-semibold text-center text-2xl">Struktur Organisasi</h1>
+    </div>
+    <div class="w-full lg:w-1/2 mx-auto">
+        <img src="{{ asset('images/struktur.jpg') }}" alt="Struktur Organisasi">
+    </div>
 </x-guest-layout>
--- resources/views/pages/user/profile/visi-misi.blade.php
+++ resources/views/pages/user/profile/visi-misi.blade.php
@@ -1,3 +1,15 @@
 <x-guest-layout>
-    <h1 class="text-4xl">Ini Visi & Misi</h1>
+    <div class="w-full lg:w-1/2 mx-auto mb-8">
+        <h1 class="font-semibold text-center text-2xl">Visi & Misi</h1>
+    </div>
+    <div class="w-full lg:w-1/2 mx-auto mb-4">
+        <h2 class="font-semibold text-left text-xl mb-2">Visi</h3>
+        <p class="font-medium text-lg mb-4">
+            Terwujudnya pembangunan kabupaten bireuen yang adil, makmur, aman, damai dan sejahtera berlandaskan syariat islam
+        </p>
+        <h2 class="font-semibold text-left text-xl mb-2">Misi</h3>
+        <p class="font-medium text-lg mb-4">
+            Menata dan pengembangan potensi pemuda, olahraga dan pariwisata
+        </p>
+    </div>   
 </x-guest-layout>
