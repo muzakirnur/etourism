33b15522e90df22829fe59844f2136b1bb2982fd
Muzakir Nur <muzakir.nurr@gmail.com>
2024-01-19 16:57:34 +0700

    Menambahkan Background login dan register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,41 +1,43 @@
 <x-authentication-layout>
-    <h1 class="text-3xl text-slate-800 dark:text-slate-100 font-bold mb-6">{{ __('Selamat Datang!') }} ✨</h1>
-    @if (session('status'))
-        <div class="mb-4 font-medium text-sm text-green-600">
-            {{ session('status') }}
-        </div>
-    @endif   
-    <!-- Form -->
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
-        <div class="space-y-4">
-            <div>
-                <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" type="email" name="email" :value="old('email')" required autofocus />                
+    <div class="bg-white p-4 rounded-lg">
+        <h1 class="text-3xl text-slate-800 dark:text-slate-100 font-bold mb-6">{{ __('Selamat Datang!') }} ✨</h1>
+        @if (session('status'))
+            <div class="mb-4 font-medium text-sm text-green-600">
+                {{ session('status') }}
             </div>
-            <div>
-                <x-jet-label for="password" value="{{ __('Password') }}" />
-                <x-jet-input id="password" type="password" name="password" required autocomplete="current-password" />                
-            </div>
-        </div>
-        <div class="flex items-center justify-between mt-6">
-            @if (Route::has('password.request'))
-                <div class="mr-1">
-                    <a class="text-sm underline hover:no-underline" href="{{ route('password.request') }}">
-                        {{ __('Forgot Password?') }}
-                    </a>
+        @endif   
+        <!-- Form -->
+        <form method="POST" action="{{ route('login') }}">
+            @csrf
+            <div class="space-y-4">
+                <div>
+                    <x-jet-label for="email" value="{{ __('Email') }}" />
+                    <x-jet-input id="email" type="email" name="email" :value="old('email')" required autofocus />                
                 </div>
-            @endif            
-            <x-jet-button class="ml-3">
-                {{ __('Sign in') }}
-            </x-jet-button>            
-        </div>
-    </form>
-    <x-jet-validation-errors class="mt-4" />   
-    <!-- Footer -->
-    <div class="pt-5 mt-6 border-t border-slate-200">
-        <div class="text-sm">
-            {{ __('Belum Punya Akun ?') }} <a class="font-medium text-indigo-500 hover:text-indigo-600" href="{{ route('register') }}">{{ __('Sign Up') }}</a>
+                <div>
+                    <x-jet-label for="password" value="{{ __('Password') }}" />
+                    <x-jet-input id="password" type="password" name="password" required autocomplete="current-password" />                
+                </div>
+            </div>
+            <div class="flex items-center justify-between mt-6">
+                @if (Route::has('password.request'))
+                    <div class="mr-1">
+                        <a class="text-sm underline hover:no-underline" href="{{ route('password.request') }}">
+                            {{ __('Forgot Password?') }}
+                        </a>
+                    </div>
+                @endif            
+                <x-jet-button class="ml-3">
+                    {{ __('Sign in') }}
+                </x-jet-button>            
+            </div>
+        </form>
+        <x-jet-validation-errors class="mt-4" />   
+        <!-- Footer -->
+        <div class="pt-5 mt-6 border-t border-slate-200">
+            <div class="text-sm">
+                {{ __('Belum Punya Akun ?') }} <a class="font-medium text-indigo-500 hover:text-indigo-600" href="{{ route('register') }}">{{ __('Sign Up') }}</a>
+            </div>
         </div>
     </div>
 </x-authentication-layout>
