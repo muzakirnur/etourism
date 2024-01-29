cfeebab5869c6f3f8ee65e8eb8fa00488be8fdc3
Muzakir Nur <muzakir.nurr@gmail.com>
2024-01-29 17:21:08 +0700

    removing user Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::factory(1000)->create();
+        // User::factory(1000)->create();
         $this->call([
             DashboardTableSeeder::class,
         ]);
