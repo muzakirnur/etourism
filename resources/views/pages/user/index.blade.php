1d2e096cc1f4d8179b5d1e944dd6f82ddc00661d
Muzakir Nur <muzakir.nurr@gmail.com>
2024-02-17 16:26:48 +0700

    Menambahkan Icon pada Home

--- resources/views/pages/user/index.blade.php
+++ resources/views/pages/user/index.blade.php
@@ -2,16 +2,25 @@
     <div class="max-w-screen-xl px-4 py-8 mx-auto text-center lg:py-16 lg:px-6">
         <dl class="grid max-w-screen-md gap-8 mx-auto text-gray-900 sm:grid-cols-3 dark:text-white">
             <div class="flex flex-col items-center justify-center">
-                <dt class="mb-2 text-3xl md:text-4xl font-extrabold">{{ $wisata }}</dt>
-                <dd class="font-light text-gray-500 dark:text-gray-400">Wisata</dd>
+                <div class="w-full bg-indigo-200 rounded-lg shadow-lg p-4">
+                    <i class="fa-solid fa-map-location-dot text-indigo-600 text-3xl"></i>
+                    <dd class="font-light text-gray-500 dark:text-gray-400">Wisata</dd>
+                    <dt class="mb-2 text-3xl md:text-4xl font-extrabold">{{ $wisata }}</dt>
+                </div>
             </div>
             <div class="flex flex-col items-center justify-center">
-                <dt class="mb-2 text-3xl md:text-4xl font-extrabold">{{ $hotel }}</dt>
-                <dd class="font-light text-gray-500 dark:text-gray-400">Akomodasi</dd>
+                <div class="w-full bg-indigo-200 rounded-lg shadow-lg p-4">
+                    <i class="fa-solid fa-hotel text-indigo-600 text-3xl"></i>
+                    <dd class="font-light text-gray-500 dark:text-gray-400">Hotel</dd>
+                    <dt class="mb-2 text-3xl md:text-4xl font-extrabold">{{ $hotel }}</dt>
+                </div>
             </div>
             <div class="flex flex-col items-center justify-center">
-                <dt class="mb-2 text-3xl md:text-4xl font-extrabold">{{ $user }}</dt>
-                <dd class="font-light text-gray-500 dark:text-gray-400">Pengguna</dd>
+                <div class="w-full bg-indigo-200 rounded-lg shadow-lg p-4">
+                    <i class="fa-solid fa-users text-indigo-600 text-3xl"></i>
+                    <dd class="font-light text-gray-500 dark:text-gray-300">Pengguna</dd>
+                    <dt class="mb-2 text-3xl md:text-4xl font-extrabold">{{ $user }}</dt>
+                </div>
             </div>
         </dl>
     </div>
