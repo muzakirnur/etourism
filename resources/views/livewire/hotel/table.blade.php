a346ba2060e6ce1466c8a0c53182128dc8cc825d
Muzakir Nur <muzakir.nurr@gmail.com>
2024-05-17 14:15:15 +0700

    fixing error null path

--- resources/views/livewire/hotel/table.blade.php
+++ resources/views/livewire/hotel/table.blade.php
@@ -14,7 +14,8 @@
     </div>
     <table class="table-auto w-full dark:text-slate-300">
         <!-- Table header -->
-        <thead class="text-xs uppercase text-slate-400 dark:text-slate-500 bg-slate-50 dark:bg-slate-700 dark:bg-opacity-50 rounded-sm">
+        <thead
+            class="text-xs uppercase text-slate-400 dark:text-slate-500 bg-slate-50 dark:bg-slate-700 dark:bg-opacity-50 rounded-sm">
             <tr>
                 <th class="p-2">
                     <div class="font-semibold text-left">Gambar</div>
@@ -33,26 +34,29 @@
         <!-- Table body -->
         <tbody class="text-sm font-medium divide-y divide-slate-100 dark:divide-slate-700">
             <!-- Row -->
-            @foreach ($data as $row) 
-            <tr>
-                <td class="p-2">
-                    @if ($row->pictures)
-                    <img class="w-96 h-48" src="{{ asset('storage/'.$row->pictures->first()->path) }}" alt="{{ $row->name }}">
-                    @endif
-                </td>
-                <td class="p-2">
-                    <div class="text-center">{{ $row->nama }}</div>
-                </td>
-                <td class="p-2">
-                    <div class="text-center">{{ $row->alamat }}</div>
-                </td>
-                <td class="p-2">
-                    <div class="w-full text-end">
-                        <a href="{{route('hotel.edit', $row)}}" class="btn bg-indigo-500 hover:bg-indigo-600 text-white whitespace-nowrap">Edit</a>
-                        <button class="btn bg-red-500 hover:bg-red-600 text-white whitespace-nowrap" wire:click='delete({{ $row->id }})'>Delete</button>
-                    </div>
-                </td>
-            </tr>
+            @foreach ($data as $row)
+                <tr>
+                    <td class="p-2">
+                        @if ($row->pictures)
+                            <img class="w-96 h-48" src="{{ asset('storage/' . $row->pictures->first()?->path) }}"
+                                alt="{{ $row->name }}">
+                        @endif
+                    </td>
+                    <td class="p-2">
+                        <div class="text-center">{{ $row->nama }}</div>
+                    </td>
+                    <td class="p-2">
+                        <div class="text-center">{{ $row->alamat }}</div>
+                    </td>
+                    <td class="p-2">
+                        <div class="w-full text-end">
+                            <a href="{{ route('hotel.edit', $row) }}"
+                                class="btn bg-indigo-500 hover:bg-indigo-600 text-white whitespace-nowrap">Edit</a>
+                            <button class="btn bg-red-500 hover:bg-red-600 text-white whitespace-nowrap"
+                                wire:click='delete({{ $row->id }})'>Delete</button>
+                        </div>
+                    </td>
+                </tr>
             @endforeach
         </tbody>
     </table>
