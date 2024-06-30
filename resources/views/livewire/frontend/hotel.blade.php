94d54d726c27a79a452fd9fa3179845150d1376c
Muzakir Nur <muzakir.nurr@gmail.com>
2024-07-01 01:58:53 +0700

    perbaikan tampilan

--- resources/views/livewire/frontend/hotel.blade.php
+++ resources/views/livewire/frontend/hotel.blade.php
@@ -1,21 +1,22 @@
 <div class="w-full p-4 mb-4">
     <h2 class="font-extrabold text-2xl text-center mb-8">Rekomendasi Akomodasi</h2>
-    <div class="w-full md:grid md:grid-cols-4 md:gap-4 mb-4">
+    <div class="w-full md:grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:gap-4 mb-4">
         @foreach ($data as $row)
             <div
-                class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mb-4">
-                <a href="{{ route('hotel.user.show', Crypt::encrypt($row->id)) }}">
-                    <img class="rounded-t-lg" src="{{ asset('storage/' . $row->pictures->first()?->path) }}"
-                        alt="{{ $row->nama }}" />
+                class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mb-4 h-80">
+                <a href="{{ route('user.wisata.show', Crypt::encrypt($row->id)) }}">
+                    <img class="rounded-t-lg h-1/2 object-cover w-full"
+                        src="{{ asset('storage/' . $row->pictures->first()?->path) }}" alt="{{ $row->nama }}" />
                 </a>
-                <div class="p-5">
-                    <a href="{{ route('hotel.user.show', Crypt::encrypt($row->id)) }}">
-                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                <div class="p-2">
+                    <a href="{{ route('user.wisata.show', Crypt::encrypt($row->id)) }}">
+                        <h5 class="mb-2 md:text-md lg:text-lg font-bold tracking-tight text-gray-900 dark:text-white">
                             {{ $row->nama }}</h5>
                     </a>
-                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 overflow-hidden">{{ $row->short_desc }}
+                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 overflow-hidden">
+                        {{ $row->short_desc }}
                     </p>
-                    <a href="{{ route('hotel.user.show', Crypt::encrypt($row->id)) }}"
+                    <a href="{{ route('user.wisata.show', Crypt::encrypt($row->id)) }}"
                         class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                         Lihat
                         <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true"
