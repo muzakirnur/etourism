e92541f4842f0e70d2f0ad19de5f294a4a1372ed
Muzakir Nur <muzakir.nurr@gmail.com>
2024-07-24 19:41:22 +0700

    menambahkan data table

--- resources/views/livewire/user/index.blade.php
+++ resources/views/livewire/user/index.blade.php
@@ -14,13 +14,26 @@
     </div>
     <table class="table-auto w-full dark:text-slate-300">
         <!-- Table header -->
-        <thead class="text-xs uppercase text-slate-400 dark:text-slate-500 bg-slate-50 dark:bg-slate-700 dark:bg-opacity-50 rounded-sm">
+        <thead
+            class="text-xs uppercase text-slate-400 dark:text-slate-500 bg-slate-50 dark:bg-slate-700 dark:bg-opacity-50 rounded-sm">
             <tr>
                 <th class="p-2">
                     <div class="font-semibold text-left">Nama</div>
                 </th>
                 <th class="p-2">
                     <div class="font-semibold text-center">Email</div>
+                </th>
+                <th class="p-2">
+                    <div class="font-semibold text-center">Umur</div>
+                </th>
+                <th class="p-2">
+                    <div class="font-semibold text-center">Alamat</div>
+                </th>
+                <th class="p-2">
+                    <div class="font-semibold text-center">Pekerjaan</div>
+                </th>
+                <th class="p-2">
+                    <div class="font-semibold text-center">Jenis Kelamin</div>
                 </th>
                 <th class="p-2">
                     <div class="font-semibold text-end">Action</div>
@@ -30,20 +43,33 @@
         <!-- Table body -->
         <tbody class="text-sm font-medium divide-y divide-slate-100 dark:divide-slate-700">
             <!-- Row -->
-            @foreach ($data as $row) 
-            <tr>
-                <td class="p-2">
-                    <div class="text-left">{{ $row->name }}</div>
-                </td>
-                <td class="p-2">
-                    <div class="text-center">{{ $row->email }}</div>
-                </td>
-                <td class="p-2">
-                    <div class="w-full text-end">
-                        <button class="btn bg-red-500 hover:bg-red-600 text-white whitespace-nowrap" wire:click='delete({{ $row->id }})'>Delete</button>
-                    </div>
-                </td>
-            </tr>
+            @foreach ($data as $row)
+                <tr>
+                    <td class="p-2">
+                        <div class="text-left">{{ $row->name }}</div>
+                    </td>
+                    <td class="p-2">
+                        <div class="text-center">{{ $row->email }}</div>
+                    </td>
+                    <td class="p-2">
+                        <div class="text-center">{{ $row->umur }}</div>
+                    </td>
+                    <td class="p-2">
+                        <div class="text-center">{{ $row->alamat }}</div>
+                    </td>
+                    <td class="p-2">
+                        <div class="text-center">{{ $row->pekerjaan }}</div>
+                    </td>
+                    <td class="p-2">
+                        <div class="text-center">{{ $row->jenis_kelamin }}</div>
+                    </td>
+                    <td class="p-2">
+                        <div class="w-full text-end">
+                            <button class="btn bg-red-500 hover:bg-red-600 text-white whitespace-nowrap"
+                                wire:click='delete({{ $row->id }})'>Delete</button>
+                        </div>
+                    </td>
+                </tr>
             @endforeach
         </tbody>
     </table>
